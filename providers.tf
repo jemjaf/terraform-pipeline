13f0b956b2ff2008f0fa71a168e142c3ac9addaf
Jean Reyes <jemjaf@Dream-Machine.localdomain>
2026-05-07 20:37:23 -0500

    first commit

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.44.0"
+    }
+  }
+}
+
+provider "aws" {
+    default_tags {
+        tags = local.tags
+    }
+}
